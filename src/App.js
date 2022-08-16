b2cb96057bcc46d8a78e3d5a69d750b8c07c9dc9
abdessamader <abdessamaderrifi46@gmail.com>
2022-08-16 10:32:17 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import './App.css'
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [solution, setSolution] = useState(null)
+  useEffect(() => {
+    fetch(' http://localhost:8000/solutions')
+    .then(res => res.json())
+    .then(data => {
+      // random integer between 0 & 14
+    })
+  }, [])
+
+
+
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
